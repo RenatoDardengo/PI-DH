34171ed32b4417ad65d2cdd2d5bff195c4a20f5a
RenatoDardengo <rdardengo01@gmail.com>
2022-07-08 21:56:39 -0300

    Criado servidor

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const methodOverride = require ("method-override");
 
+app.use(express.static(__dirname + "/public"));
+app.set ("view engine" , "ejs");
+app.set("views", __dirname + "/src/views");
+app.use(methodOverride("_method"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+
+app.listen(port, ()=>{
+    console.log (`Servidor rodando no endereço http://localhost:${port}`)
+})
