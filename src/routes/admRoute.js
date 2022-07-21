47eb9145336c437d00fa3682fbdedbbd89e40500
RenatoDardengo <rdardengo01@gmail.com>
2022-07-21 20:37:14 -0300

    Subindo tela login_Admin e correções pg Administrador

--- src/routes/admRoute.js
+++ src/routes/admRoute.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const admController = require("../controllers/AdmController")
 
 router.get("/", admController.index)
+//rotas produtos
 router.get("/product", admController.show)
 router.get("/product/create", admController.create)
 router.get("/product/edit", admController.edit)
 router.get("/product/delete", admController.delete)
+
 
 module.exports=router;
