34634e460c7d75946cd719070fdba73ac5d1ff6a
RenatoDardengo <rdardengo01@gmail.com>
2022-09-21 23:20:24 -0300

    Concluido carrinho de compras

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -12,55 +12,56 @@ const confirmation = {
     show: async (req, res) => {
 
         const {id}= req.params;
-        
-
         var newProduct= await Product.findOne({
             where: { id: id }
         });
-
-        var valueProd = parseFloat(newProduct.saleValue).toFixed(2)
-
+        
         if (req.cookies.idProd) {
-            var ids = req.cookies.idProd
-            ids.push ({'id':parseInt (id),'qtde':1, 'value':valueProd})
-            console.log(ids)
-            res.cookie("idProd", ids)
-            var onlyId=[]
-            var only= ids.map(Element=>{
-                Element.id
-                onlyId.push(Element.id)
+            var cartProducts = req.cookies.idProd
+            const found = cartProducts.find(e => e.id==id);
+            if(found){
+                cartProducts.forEach(element => {
+                    if(element.id==id){
+                        element.qtde= parseInt(element.qtde + 1)
+                        element.value = parseFloat (newProduct.saleValue)*parseInt(element.qtde)
+                    }
+                });
+                
+            }else{
+                cartProducts.push ({'id':parseInt (id),'qtde':1, 'value':parseFloat (newProduct.saleValue).toFixed(2)})
 
-             })
-           
-             console.log(only)
-
+            }
+            
+            res.cookie("idProd", cartProducts)
+            var onlyId = cartProducts.map((e)=>e.id)
             var productSelected = await Product.findAll({
                 where: { id: { [Op.in]: onlyId } }
             });
 
         } else {
-            res.cookie("idProd", [{'id':parseInt(id),'qtde':1,'value':valueProd}]);
+            res.cookie("idProd", [{'id':parseInt(id),'qtde':1,'value':parseFloat (newProduct.saleValue).toFixed(2)}]);
             var productSelected = await Product.findOne({
                 where: { id: id }
             });
         }
+
         if(productSelected.length > 0){
-            productSelected.map(productSelected => 
-            productSelected.img = files.base64Encode(__dirname + "/../../uploads/" + productSelected.img),)
-            productSelected.map(productSelected => 
-                productSelected.saleValue = parseFloat(productSelected.saleValue).toFixed(2));
-
-
+            productSelected.map(productSelected => productSelected.img = files.base64Encode(__dirname + "/../../uploads/" + productSelected.img));
+            productSelected.map(productSelected => productSelected.saleValue = parseFloat(productSelected.saleValue).toFixed(2));
 
         }else{
-            const imgProduct = {...productSelected,
-                img:files.base64Encode(upload.path + productSelected.img)
-            }
+            const imgProduct = {...productSelected, img:files.base64Encode(upload.path + productSelected.img)}
     
-            productSelected.img=imgProduct.img
+            productSelected.img=imgProduct.img;
             productSelected.saleValue = parseFloat(productSelected.saleValue).toFixed(2)
         }
-        
+cartProducts.sort((x,y)=> x.id -y.id)
+        for (let i = 0; i < productSelected.length; i++) {
+            productSelected[i].Qtde= cartProducts[i].qtde
+            productSelected[i].valueTotal= parseFloat(cartProducts[i].value).toFixed(2)
+            
+            }
+      
         
         const user = req.session.name
         return res.render("cartuser", { title: "Meu Carrinho", productSelected, user })
